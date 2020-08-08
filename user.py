34756c270d85de0569462e425e542d9ff0b211e9
SarahAbdullah <dev.abdullah0615@gmail.com>
2020-08-08 16:54:43 -0400

    Updated

---
+++ user.py
@@ -0,0 +1,21 @@
+class User:
+    def __init__(self):
+        self.random()
+        #self.latest_ID = 1
+    
+    def get_ran(self):
+        return self.rand
+    
+    def set_ran(self):
+
+    def ran(self):
+        current_ran = input (f"What is your current mood?")
+        ran_id = self.latest_ID
+        self.randomlist.append
+        (current_ran, ran_id)
+
+        
+            
+           
+
+            
