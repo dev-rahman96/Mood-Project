61747b4e2860a027c7d85633f9286ac21eb6be39
dev.rahman96 <dev.rahman96@gmail.com>
2020-08-08 16:48:51 -0400

    template for functions

--- random.py
+++ random.py
@@ -4,3 +4,24 @@ import os
 import pickle
 from user import User
 
+class Random:
+  def __init__(self):
+    self.user = {}
+    self.latest_ID = 1
+
+
+  def api_call(self):
+    pass
+
+  def create_user(self):
+    pass
+
+
+  def update_user(self):
+    pass
+
+  def read(self):
+    pass
+
+  def delete_user(self):
+    pass
